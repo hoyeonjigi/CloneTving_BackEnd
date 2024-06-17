c0f5f5f220f1cc10f3e7338b52e4e11e21273db5
Yu Ho Yeon <zmfmsh46@gmail.com>
2024-06-18 01:08:46 +0900

    init

---
+++ hoyeonjigi/src/main/java/site/hoyeonjigi/HoyeonjigiApplication.java
@@ -0,0 +1,13 @@
+package site.hoyeonjigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoyeonjigiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoyeonjigiApplication.class, args);
+	}
+
+}
---
+++ hoyeonjigi/src/test/java/site/hoyeonjigi/HoyeonjigiApplicationTests.java
@@ -0,0 +1,16 @@
+package site.hoyeonjigi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HoyeonjigiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
