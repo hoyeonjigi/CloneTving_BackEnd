8e5b9eed17ef99566ca5849685a07faa66f23e7c
minsik2434 <minsik2434@gmail.com>
2024-06-22 18:17:44 +0900

    Feat : 컨텐츠 조회 로직 구현

--- hoyeonjigi/src/main/java/site/hoyeonjigi/repository/content/ContentRepositoryImpl.java
+++ hoyeonjigi/src/main/java/site/hoyeonjigi/repository/content/ContentRepositoryImpl.java
@@ -2,11 +2,15 @@ package site.hoyeonjigi.repository.content;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import site.hoyeonjigi.entity.QContentGenre;
+import site.hoyeonjigi.entity.QGenre;
 import site.hoyeonjigi.entity.content.Content;
 import site.hoyeonjigi.entity.content.QContent;
 import site.hoyeonjigi.entity.content.QDrama;
@@ -14,6 +18,8 @@ import site.hoyeonjigi.entity.content.QMovie;
 
 import java.util.List;
 
+import static site.hoyeonjigi.entity.QContentGenre.*;
+import static site.hoyeonjigi.entity.QGenre.genre;
 import static site.hoyeonjigi.entity.content.QContent.content;
 import static site.hoyeonjigi.entity.content.QDrama.drama;
 import static site.hoyeonjigi.entity.content.QMovie.movie;
@@ -22,14 +28,18 @@ public class ContentRepositoryImpl implements ContentCustomRepository{
     QContent qContent = content;
     QMovie qMovie = movie;
     QDrama qDrama = drama;
+    QContentGenre qContentGenre = contentGenre;
+    QGenre qGenre = genre;
     private final JPAQueryFactory queryFactory;
     public ContentRepositoryImpl(EntityManager em){
         this.queryFactory = new JPAQueryFactory(em);
     }
     @Override
-    public Page<Content> findContentsByTypeAndSort(String type, String sort, Pageable pageable) {
-        QueryResults<Content> results = queryFactory.selectFrom(content)
-                .where(content.dType.eq(type))
+    public Page<Content> findContentsByTypeAndSortOrGenre(String type, String sort,String genreName, String title, Pageable pageable) {
+        QueryResults<Content> results = queryFactory.select(content).distinct().from(content)
+                .join(content.contentGenres, contentGenre)
+                .join(contentGenre.genre, genre)
+                .where(filterType(type), filterGenreName(genreName), filterTitle(title))
                 .orderBy(getOrderBy(sort))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -39,6 +49,25 @@ public class ContentRepositoryImpl implements ContentCustomRepository{
         return new PageImpl<>(content, pageable, totalCount);
     }
 
+    private BooleanExpression filterType(String type){
+        if(type==null){
+            return null;
+        }
+        return content.dType.eq(type);
+    }
+    private BooleanExpression filterGenreName(String genreName){
+        if(genreName == null){
+            return null;
+        }
+        return genre.name.eq(genreName);
+    }
+
+    private BooleanExpression filterTitle(String title){
+        if(title == null){
+            return null;
+        }
+        return content.contentTitle.contains(title);
+    }
     private OrderSpecifier<?>[] getOrderBy(String sort) {
         OrderSpecifier<?>[] orders;
         if (sort.equals("popular")) {
