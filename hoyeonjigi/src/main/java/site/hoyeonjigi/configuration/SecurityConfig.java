3e0aad0d44c3254bc9ea6f52fbb9064a6b38c168
Yu Ho Yeon <zmfmsh46@gmail.com>
2024-08-09 00:55:21 +0900

    cors add domain

--- hoyeonjigi/src/main/java/site/hoyeonjigi/configuration/SecurityConfig.java
+++ hoyeonjigi/src/main/java/site/hoyeonjigi/configuration/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://clone-tving.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH","DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Content-Type", "Authorization", "X-XSRF-token"));
         configuration.setAllowCredentials(true);
